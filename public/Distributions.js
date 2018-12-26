b2f7bb9df833115bf87e220198fc9b1bbb33d3b4
Marek Pelka <pelkam95@gmail.com>
2018-12-26 21:51:26 +0100

    Simple user input

--- public/Distributions.js
+++ public/Distributions.js
@@ -1,5 +1,5 @@
 // Laplace vars
-var mu = 0.0
+var mu = 0
 var b = 1
 
 // Gaussian vars
@@ -9,6 +9,9 @@ var sigma = 1
 var a = 1
 
 //util vars
+var muInput, bInput, sigmaInput, aInput, recalculateButton
+var laplaceSumDiv, gaussianSumDiv, rayleighSumDiv, maxwellSumDiv
+
 var first = true
 chartColors = {
     red: 'rgb(255, 99, 132)',
@@ -17,13 +20,19 @@ chartColors = {
     green: 'rgb(75, 192, 192)',
     blue: 'rgb(54, 162, 235)',
     purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(201, 203, 207)'
+    grey: 'rgb(201, 203, 207)',
+    black: 'rgb(12, 44, 27)',
 };
+
+var xMin = -10
+var xMax = 10
+var xStep = 1 / 10
 
 // p5.js function - will run once
 function setup() {
 
-    window['chart'] = new Chart(document.getElementById('canvas1'), {
+    noCanvas();
+    window['chartCanvas1'] = new Chart(document.getElementById('canvas1'), {
         type: 'line',
         data: {},
         options: {
@@ -36,39 +45,77 @@ function setup() {
         }
 
     })
+
+    // Controls at the bottom of the chart
+    muInput = createInput(mu.toString())
+    muInput.parent('mu')
+    bInput = createInput(b.toString())
+    bInput.parent('b')
+    sigmaInput = createInput(sigma.toString())
+    sigmaInput.parent('sigma')
+    aInput = createInput(a.toString())
+    aInput.parent('a')
+
+    xMinInput = createInput(xMin.toString())
+    xMinInput.parent('xMin')
+    xMaxInput = createInput(xMax.toString())
+    xMaxInput.parent('xMax')
+    xStepInput = createInput(xStep.toString())
+    xStepInput.parent('xStep')
+
+    recalculateButton = createButton('recalculate')
+    recalculateButton.parent('inputs')
+    recalculateButton.mousePressed(recalculate);
+
+    laplaceSumDiv = createElement('tr')
+    laplaceSumDiv.parent(controlTable)
+    gaussianSumDiv = createElement('tr')
+    gaussianSumDiv.parent(controlTable)
+    rayleighSumDiv = createElement('tr')
+    rayleighSumDiv.parent(controlTable)
+    maxwellSumDiv = createElement('tr')
+    maxwellSumDiv.parent(controlTable)
 }
 
 // p5.js function - will run in a loop
 function draw() {
     if (first) {
-        // Map in which every entry have 2 fields - data a sum 
         distribution = new Map([
-            ["Laplace", calculateDistribution(laplace)],
-            ["Gaussian", calculateDistribution(gaussian)],
-            ["Rayleigh", calculateDistribution(rayleigh)],
-            ["Maxwell", calculateDistribution(maxwell)]
+            ["Laplace", laplace],
+            ["Gaussian", gaussian],
+            ["Rayleigh", rayleigh],
+            ["Maxwell", maxwell],
+            ["LaplaceIntegral", integralWrapper(laplace)],
+            ["GaussianIntegral", integralWrapper(gaussian)],
+            ["RayleighIntegral", integralWrapper(rayleigh)],
+            ["MaxwellIntegral", integralWrapper(maxwell)],
         ])
-        console.log("Laplace = " + distribution.get("Laplace").sum)
-        console.log("Gaussian = " + distribution.get("Gaussian").sum)
-        console.log("Rayleigh = " + distribution.get("Rayleigh").sum)
-        console.log("Maxwell = " + distribution.get("Maxwell").sum)
         distribution.forEach(addNewDataset);
+        updateSums()
         first = false
     }
 }
 
-function calculateDistribution(fun) {
-    var listData = []
-    var sum = 0
-    for (let iter = -1000; iter < 1000; iter++) {
-        var xSize = (1 / 100)
-        x = iter * xSize
-        y = math.integrate(fun, -1000 * xSize, x)
-
-        sum += y * xSize
-        listData.push({ x: x, y: 1 - y })
+function integralWrapper(fun) {
+    return function (x) {
+        return 1 - math.integrate(fun, xMin, x, xStep)
     }
-    return { data: listData, sum: sum }
+}
+
+function calculateDistribution(fun, dataset) {
+
+    // Next 4 lines are to make sure that stepping from xMin to xMax we will hit 0 - very important for one-sided distributions
+    var x = 0
+    while (x > xMin) {
+        x -= xStep
+    }
+    // '- xStep' just for Chart.js to have domain up to xMax, otherwise it is extended to xMax + 2
+    while (x < xMax - xStep) {
+        x += xStep
+        y = fun(x)
+        dataset.push({ x: x, y: y })
+    }
+    window['chartCanvas1'].update()
 }
 
 function addNewDataset(value, key, map) {
@@ -80,15 +127,123 @@ function addNewDataset(value, key, map) {
 
     var newDataset = {
         label: key,
-        data: value.data,
+        data: [],
         fill: false,
         borderColor: color,
         backgroundColor: color,
         borderWidth: 3,
         pointRadius: 0
     }
-    window['chart'].data.datasets.push(newDataset)
-    window['chart'].update()
+    window['chartCanvas1'].data.datasets.push(newDataset)
+    calculateDistribution(value, getDataset(key).data)
+    window['chartCanvas1'].update()
+}
+
+//////////////////////////
+// Logic for recalculating distributions after user input
+////////////////////////// 
+
+function recalculate() {
+
+    var changedParams = getChangedParams()
+    whatDatasetToRecalculate(changedParams)
+}
+
+function getChangedParams() {
+
+    var output = []
+    var newParam = getParamFromInput(muInput)
+    if (newParam != mu) { mu = newParam; output.push('mu') }
+    newParam = getParamFromInput(bInput)
+    if (newParam != b) { b = newParam; output.push('b') }
+    newParam = getParamFromInput(sigmaInput)
+    if (newParam != sigma) { sigma = newParam; output.push('sigma') }
+    newParam = getParamFromInput(aInput)
+    if (newParam != a) { a = newParam; output.push('a') }
+
+    newParam = getParamFromInput(xMinInput)
+    if (newParam != xMin) { xMin = newParam; output.push('xMin') }
+    newParam = getParamFromInput(xMaxInput)
+    if (newParam != xMax) { xMax = newParam; output.push('xMax') }
+    newParam = getParamFromInput(xStepInput)
+    if (newParam != xStep) { xStep = newParam; output.push('xStep') }
+    return output
+}
+
+function getParamFromInput(input) {
+    return parseFloat(input.value(), 10);
+}
+
+function whatDatasetToRecalculate(changedParamsList) {
+    changedParamsList.forEach(param => {
+
+        switch (param) {
+            case 'mu':
+                recalculateDataset('Gaussian')
+                recalculateDataset('Laplace')
+                break;
+            case 'b':
+                recalculateDataset('Laplace')
+                break;
+            case 'sigma':
+                recalculateDataset('Gaussian')
+                recalculateDataset('Rayleigh')
+                break;
+            case 'a':
+                recalculateDataset('Maxwell')
+                break;
+            case 'xMin':
+            case 'xMax':
+            case 'xStep':
+                recalculateAllDatasets()
+                break;
+        }
+    });
+    updateSums()
+    window['chartCanvas1'].update()
+}
+
+function recalculateDataset(label) {
+    filterDatasetByLabel(label)
+    filterDatasetByLabel(label + 'Integral')
+
+    addNewDatasetFromDistribution(label)
+    addNewDatasetFromDistribution(label + 'Integral')
+}
+
+function recalculateAllDatasets() {
+    
+    recalculateDataset('Gaussian')
+    recalculateDataset('Rayleigh')
+    recalculateDataset('Laplace')
+    recalculateDataset('Maxwell')
+}
+
+function getDataset(label) {
+    return window['chartCanvas1'].data.datasets.find(dataset => dataset.label === label)
+}
+
+function filterDatasetByLabel(label) {
+    var dataset = getDataset(label)
+    // Cool way for returning a color back to pool
+    chartColors[Math.random()] = dataset.backgroundColor
+    window['chartCanvas1'].data.datasets = window['chartCanvas1'].data.datasets.filter(dataset => dataset.label !== label)
+}
+
+function addNewDatasetFromDistribution(label) {
+    addNewDataset(distribution.get(label), label)
+}
+
+function updateSums() {
+    laplaceSumDiv.html('<td>Laplace: </td><td>' + getSumFromLabel('Laplace') + '</td>')
+    gaussianSumDiv.html('<td>Gaussian: </td><td>' + getSumFromLabel('Gaussian') + '</td>')
+    rayleighSumDiv.html('<td>Rayleigh: </td><td>' + getSumFromLabel('Rayleigh') + '</td>')
+    maxwellSumDiv.html('<td>Maxwell: </td><td>' + getSumFromLabel('Maxwell') + '</td>')
+}
+
+function getSumFromLabel(label) {
+    integralLabel = label + 'Integral'
+    return (getDataset(integralLabel).data[getDataset(integralLabel).data.length - 1].y + 1).toString()
 }
 
 //////////////////////////
@@ -166,7 +321,7 @@ function integrate(f, start, end, step) {
  * @param {Object} math
  * @param {Object} [scope]
  */
-integrate.transform = function(args, math, scope) {
+integrate.transform = function (args, math, scope) {
     // determine the variable name
     if (!args[1].isSymbolNode) {
         throw new Error('Second argument must be a symbol')
@@ -185,7 +340,7 @@ integrate.transform = function(args, math, scope) {
     // construct a function which evaluates the first parameter f after applying
     // a value for parameter x.
     const fnCode = args[0].compile()
-    const f = function(x) {
+    const f = function (x) {
         fnScope[variable] = x
         return fnCode.eval(fnScope)
     }
